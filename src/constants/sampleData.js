a3f7afa6bd3ef48d939bf7266ce3b09cf5f771c3
Sanskar-12 <sanskarv2004@gmail.com>
2024-03-18 16:36:51 +0530

    frontend done

--- src/constants/sampleData.js
+++ src/constants/sampleData.js
@@ -194,3 +194,115 @@ export const sampleMessage = [
     createdAt: "2024-03-12T04:54:34.674Z",
   },
 ];
+
+export const dashboardData = {
+  users: [
+    {
+      name: "John Doe",
+      avatar:
+        "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+      _id: "1",
+      username: "john_doe",
+      friends: 20,
+      groups: 5,
+    },
+    {
+      name: "John Doe",
+      avatar:
+        "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+      _id: "2",
+      username: "john_boi",
+      friends: 20,
+      groups: 25,
+    },
+  ],
+  chats: [
+    {
+      name: "John Doe",
+      avatar: [
+        "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+      ],
+      _id: "1",
+      groupChat: false,
+      members: [
+        {
+          _id: "1",
+          avatar:
+            "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+        },
+        {
+          _id: "2",
+          avatar:
+            "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+        },
+      ],
+      totalMembers: 2,
+      totalMessages: 20,
+      creator: {
+        name: "John",
+        avatar:
+          "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+      },
+    },
+    {
+      name: "John Boi",
+      avatar: [
+        "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+      ],
+      _id: "2",
+      groupChat: false,
+      members: [
+        {
+          _id: "1",
+          avatar:
+            "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+        },
+        {
+          _id: "2",
+          avatar:
+            "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+        },
+      ],
+      totalMembers: 2,
+      totalMessages: 20,
+      creator: {
+        name: "John skndfk",
+        avatar:
+          "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+      },
+    },
+  ],
+  messages: [
+    {
+      attachments: [],
+      content: "Lauda ka Message hai",
+      _id: "sdfsdfdsfsdfdsfdsf",
+      sender: {
+        avatar:
+          "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+        name: "Chaman",
+      },
+      groupChat: false,
+      chat: "sdfsdfsdfsdf",
+      createdAt: "2024-03-12T04:54:34.674Z",
+    },
+    {
+      attachments: [
+        {
+          public_id: "sdsdfsdfsdfds",
+          url: "https://cdn.pixabay.com/photo/2015/03/10/17/23/youtube-667451_1280.png",
+        },
+      ],
+      content: "Lauda ka Msdfsdessage hai",
+      _id: "sdfsdsdfsdfdsfsdfdsfdsf",
+      sender: {
+        avatar:
+          "https://secure-gl.imrworldwide.com/cgi-bin/m?ca=nlsn530690&cr=crtve&ce…lsen_plc0001&ci=nlsnci2891&am=3&at=view&rt=banner&st=image&r=1710074425462",
+        name: "Chaman",
+      },
+      groupChat: true,
+      chat: "sdfsdfssdfdsfdfsdf",
+      createdAt: "2024-03-12T04:54:34.674Z",
+    },
+  ],
+};
